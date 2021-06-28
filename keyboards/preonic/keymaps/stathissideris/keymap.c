398005337d9faaa86ada7d270a910819c8f99e56
Stathis Sideris <stathis@pixelated-noise.com>
2021-06-28 23:53:36 +0300

    [keymap] Add Clojure single and double arrows and tap shifts

--- keyboards/preonic/keymaps/stathissideris/keymap.c
+++ keyboards/preonic/keymaps/stathissideris/keymap.c
@@ -37,11 +37,15 @@ enum preonic_keycodes {
   LOWER,
   RAISE,
   BACKLIT,
+
   ASCEND,
+
   WMOVE_UP,
   WMOVE_DOWN,
   WMOVE_LEFT,
-  WMOVE_RIGHT
+  WMOVE_RIGHT,
+  ARROW,
+  DARROW
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -63,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+  LSFT_T(KC_LPRN), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LSFT_T(KC_QUOT),
   KC_LCTL, KC_LALT, KC_LGUI, ASCEND,  LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
 ),
 
@@ -124,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_preonic_grid(
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL,  KC_BSPC,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_DEL,
+  ARROW,   _______, _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, DARROW,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, LCTL(KC_PGUP), LCTL(KC_PGDN),  _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
@@ -145,7 +149,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_preonic_grid(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_HASH, KC_LPRN, KC_RPRN, KC_BSPC,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_PIPE, KC_LBRC, KC_RBRC, KC_DEL,
+  ARROW,   _______, _______, _______, _______, _______, _______, _______, KC_PIPE, KC_LBRC, KC_RBRC, DARROW,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_BSLS, KC_LCBR, KC_RCBR, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
@@ -184,91 +188,103 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-        case WMOVE_UP:
-           SEND_STRING(SS_LCTL("x") SS_TAP(X_UP));
-           return false;
-           break;
-        case WMOVE_DOWN:
-           SEND_STRING(SS_LCTL("x") SS_TAP(X_DOWN));
-           return false;
-           break;
-        case WMOVE_LEFT:
-           SEND_STRING(SS_LCTL("x") SS_TAP(X_LEFT));
-           return false;
-           break;
-        case WMOVE_RIGHT:
-           SEND_STRING(SS_LCTL("x") SS_TAP(X_RIGHT));
-           return false;
-           break;
-        case QWERTY:
-          if (record->event.pressed) {
-            set_single_persistent_default_layer(_QWERTY);
-          }
-          return false;
-          break;
-        case COLEMAK:
-          if (record->event.pressed) {
-            set_single_persistent_default_layer(_COLEMAK);
-          }
-          return false;
-          break;
-        case DVORAK:
-          if (record->event.pressed) {
-            set_single_persistent_default_layer(_DVORAK);
-          }
-          return false;
-          break;
-        case LOWER:
-          if (record->event.pressed) {
-            layer_on(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
-            layer_off(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          }
-          return false;
-          break;
-        case RAISE:
-          if (record->event.pressed) {
-            layer_on(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
-            layer_off(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          }
-          return false;
-          break;
-        case ASCEND:
-          if (record->event.pressed) {
-            layer_on(_ASCEND);
-          } else {
-            layer_off(_ASCEND);
-          }
-          return false;
-          break;
-        case BACKLIT:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-            #ifdef BACKLIGHT_ENABLE
-              backlight_step();
-            #endif
-            #ifdef RGBLIGHT_ENABLE
-              rgblight_step();
-            #endif
-            #ifdef __AVR__
-            writePinLow(E6);
-            #endif
-          } else {
-            unregister_code(KC_RSFT);
-            #ifdef __AVR__
-            writePinHigh(E6);
-            #endif
-          }
-          return false;
-          break;
+   switch (keycode) {
+   case ARROW:
+      if (record->event.pressed) {
+         SEND_STRING("->");
       }
-    return true;
+      return false;
+      break;
+   case DARROW:
+      if (record->event.pressed) {
+         SEND_STRING("->>");
+      }
+      return false;
+      break;
+   case WMOVE_UP:
+      SEND_STRING(SS_LCTL("x") SS_TAP(X_UP));
+      return false;
+      break;
+   case WMOVE_DOWN:
+      SEND_STRING(SS_LCTL("x") SS_TAP(X_DOWN));
+      return false;
+      break;
+   case WMOVE_LEFT:
+      SEND_STRING(SS_LCTL("x") SS_TAP(X_LEFT));
+      return false;
+      break;
+   case WMOVE_RIGHT:
+      SEND_STRING(SS_LCTL("x") SS_TAP(X_RIGHT));
+      return false;
+      break;
+   case QWERTY:
+      if (record->event.pressed) {
+         set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+   case COLEMAK:
+      if (record->event.pressed) {
+         set_single_persistent_default_layer(_COLEMAK);
+      }
+      return false;
+      break;
+   case DVORAK:
+      if (record->event.pressed) {
+         set_single_persistent_default_layer(_DVORAK);
+      }
+      return false;
+      break;
+   case LOWER:
+      if (record->event.pressed) {
+         layer_on(_LOWER);
+         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+         layer_off(_LOWER);
+         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+   case RAISE:
+      if (record->event.pressed) {
+         layer_on(_RAISE);
+         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+         layer_off(_RAISE);
+         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+   case ASCEND:
+      if (record->event.pressed) {
+         layer_on(_ASCEND);
+      } else {
+         layer_off(_ASCEND);
+      }
+      return false;
+      break;
+   case BACKLIT:
+      if (record->event.pressed) {
+         register_code(KC_RSFT);
+#ifdef BACKLIGHT_ENABLE
+         backlight_step();
+#endif
+#ifdef RGBLIGHT_ENABLE
+         rgblight_step();
+#endif
+#ifdef __AVR__
+         writePinLow(E6);
+#endif
+      } else {
+         unregister_code(KC_RSFT);
+#ifdef __AVR__
+         writePinHigh(E6);
+#endif
+      }
+      return false;
+      break;
+   }
+   return true;
 };
 
 bool muse_mode = false;
