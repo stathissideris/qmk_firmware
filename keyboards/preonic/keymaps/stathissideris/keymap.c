d437d097a88f21bb423295330280825bf5c9d01a
Stathis Sideris <stathis@pixelated-noise.com>
2021-07-05 11:43:12 +0300

    [keymap] Add emacs window balance and magit keys

--- keyboards/preonic/keymaps/stathissideris/keymap.c
+++ keyboards/preonic/keymaps/stathissideris/keymap.c
@@ -48,8 +48,10 @@ enum preonic_keycodes {
   WHOR,
   WVER,
   WCLO,
+  WBAL,
   ARROW,
-  DARROW
+  DARROW,
+  MAGIT
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -71,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-  LSFT_T(KC_LPRN), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LSFT_T(KC_QUOT),
+  LSFT_T(KC_9), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LSFT_T(KC_QUOT),
   KC_LCTL, KC_LALT, KC_LGUI, ASCEND,  LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
 ),
 
@@ -133,7 +135,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_preonic_grid(
   KC_TILD, KC_EXLM, KC_AT,      KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS,       KC_EQL,  KC_BSPC,
   ARROW,   _______, LGUI(KC_W), _______, KC_MINS, LGUI(KC_T), _______, _______, _______, KC_UNDS,       KC_PLUS, DARROW,
-  _______, _______, _______,    _______, _______, _______,    _______, _______, _______, KC_LCBR,       KC_RCBR, _______,
+  _______, _______, _______,    _______, _______, MAGIT,      _______, _______, _______, KC_LCBR,       KC_RCBR, _______,
   _______, _______, _______,    _______, _______, _______,    _______, _______, _______, LCTL(KC_PGUP), LCTL(KC_PGDN),  _______,
   _______, _______, _______,    _______, _______, _______,    _______, _______, KC_MNXT, KC_VOLD,       KC_VOLU, KC_MPLY
 ),
@@ -152,11 +154,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_HASH, KC_LPRN, KC_RPRN, KC_BSPC,
-  ARROW,   _______, _______, _______, _______, _______, _______, _______, KC_PIPE, KC_LBRC, KC_RBRC, DARROW,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_BSLS, KC_LCBR, KC_RCBR, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,          KC_HASH, KC_LPRN, KC_RPRN, KC_BSPC,
+  ARROW,   _______, _______, _______, _______, _______, _______, _______,       KC_PIPE, KC_LBRC, KC_RBRC, DARROW,
+  _______, _______, _______, _______, _______, _______, _______, _______,       KC_BSLS, KC_LCBR, KC_RCBR, _______,
+  _______, _______, _______, _______, _______, _______, _______, LGUI(KC_SLSH), _______, _______, LGUI(KC_SLSH), _______,
+  _______, _______, _______, _______, _______, _______, _______, _______,       KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Adjust (Lower + Raise)
@@ -181,11 +183,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_ASCEND] = LAYOUT_preonic_grid(
-  _______, WMAX,       WHOR,       WVER,        _______,  _______, _______, _______, _______, _______, WCLO,    _______,
-  _______, _______,    WMOVE_UP,   _______,     _______,  _______, _______, _______, _______, _______, _______, _______,
-  _______, WMOVE_LEFT, WMOVE_DOWN, WMOVE_RIGHT, _______,  _______, _______, _______, _______, _______, _______, _______,
-  _______, _______,    _______,    LGUI(KC_EQL), _______, _______, _______, _______, _______, _______, KC_UP,   _______,
-  _______, _______,    _______,    _______,     _______,  _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT
+  _______, WMAX,    WHOR,       WVER,         _______, _______, _______, _______, _______, LGUI(KC_LPRN), WCLO,    _______,
+  _______, _______, LGUI(KC_W), _______,      _______, _______, _______, _______, _______, _______,       WBAL,    _______,
+  _______, _______, _______,    _______,      _______, _______, _______, _______, _______, _______,       _______, _______,
+  _______, _______, _______,    LGUI(KC_EQL), _______, _______, _______, _______, _______, _______,       _______,   _______,
+  _______, _______, _______,    _______,      _______, _______, _______, _______, WMOVE_LEFT, WMOVE_UP,    WMOVE_DOWN, WMOVE_RIGHT
 )
 
 
@@ -193,6 +195,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
    switch (keycode) {
+   case MAGIT:
+      if (record->event.pressed) {
+         SEND_STRING(SS_LCTL("c") SS_LCTL("g"));
+      }
+      return false;
+      break;
+   case LSFT_T(KC_9):
+      if (record->tap.count > 0) {
+         if (record->event.pressed) {
+            register_code16(KC_LPRN);
+         } else {
+            unregister_code16(KC_LPRN);
+         }
+         return false;
+      }
+      return true;
    case ARROW:
       if (record->event.pressed) {
          SEND_STRING("->");
@@ -226,6 +244,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
    case WCLO:
       if (record->event.pressed) {
          SEND_STRING(SS_LCTL("x") SS_TAP(X_0));
+      }
+      return false;
+      break;
+   case WBAL:
+      if (record->event.pressed) {
+         SEND_STRING(SS_LCTL("x") "+");
       }
       return false;
       break;
