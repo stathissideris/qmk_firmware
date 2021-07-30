795dc5169e50c048b81614ae0940a60ef4178e45
Stathis Sideris <stathis@pixelated-noise.com>
2021-07-30 20:22:12 +0300

    [keymap] More dashes, RGB, raise/lower+space

--- keyboards/preonic/keymaps/stathissideris/keymap.c
+++ keyboards/preonic/keymaps/stathissideris/keymap.c
@@ -142,9 +142,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_preonic_grid(
   KC_TILD, KC_EXLM, KC_AT,      KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS,       KC_EQL,  KC_DEL,
   ARROW,   _______, LGUI(KC_W), _______, KC_MINS, LGUI(KC_T), _______, _______, KC_PIPE, KC_UNDS,       KC_PLUS, DARROW,
-  _______, _______, _______,    _______, _______, MAGIT,      _______, _______, _______, _______,       _______, _______,
+  _______, _______, _______,    _______, KC_MINS, MAGIT,      _______, _______, _______, _______,       _______, _______,
   _______, _______, _______,    _______, KC_MINS, _______,    _______, _______, LCTL(KC_PGUP), LCTL(KC_PGDN),  _______, _______,
-  _______, _______, _______,    _______, _______, _______,    _______, _______, KC_MNXT, KC_VOLD,       KC_VOLU, KC_MPLY
+  _______, _______, _______,    _______, _______, LGUI(KC_SPC), LGUI(KC_SPC), _______, KC_MNXT, KC_VOLD,       KC_VOLU, KC_MPLY
 ),
 
 /* Raise
@@ -165,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ARROW,   _______, _______, _______, _______, _______, _______, _______,       KC_PIPE, KC_LBRC, KC_RBRC, DARROW,
   _______, _______, _______, _______, _______, _______, _______, _______,       KC_BSLS, KC_LCBR, KC_RCBR, _______,
   _______, _______, _______, _______, _______, _______, _______, LGUI(KC_SLSH), _______, _______, KC_BSLS, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______,       KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  _______, _______, _______, _______, _______, LGUI(KC_SPC), LGUI(KC_SPC), _______,       KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Adjust (Lower + Raise)
@@ -182,17 +182,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_preonic_grid(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL,
-  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______,
-  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  RGB_TOG, RGB_MOD, RGB_RMOD, RGB_VAD, RGB_VAI, RGB_HUD, RGB_HUI,   KC_F8,    KC_F9, KC_F10, RGB_M_P, KC_F12,
+  _______, RESET,   DEBUG,    _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL,
+  _______, _______, MU_MOD,   AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______,
+  _______, MUV_DE,  MUV_IN,   MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
+  _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 [_ASCEND] = LAYOUT_preonic_grid(
   _______, WMAX,    WHOR,       WVER,         _______, _______, _______, _______, _______,    LGUI(KC_LPRN), WCLO,    _______,
   _______, _______, LGUI(KC_W), _______,      _______, _______, _______, _______, CLOCK_IN,   CLOCK_OUT,       WBAL,    _______,
-  _______, _______, _______,    _______,      _______, _______, _______, _______, _______,    _______,       _______, _______,
+  _______, _______, _______,    _______,      _______, MAGIT,   _______, _______, _______,    _______,       _______, _______,
   _______, _______, _______,    LGUI(KC_EQL), _______, _______, _______, _______, _______,    _______,       _______,   _______,
   _______, _______, _______,    _______,      _______, _______, _______, _______, WMOVE_LEFT, WMOVE_UP,    WMOVE_DOWN, WMOVE_RIGHT
 )
