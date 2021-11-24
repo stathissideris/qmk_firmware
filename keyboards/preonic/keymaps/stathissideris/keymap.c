65391880f1683044d40189abe4a198f0e5efcdca
Stathis Sideris <stathis@pixelated-noise.com>
2021-11-24 15:06:30 +0200

    Remove RGB control from Adjust layer (but keep as comment)

--- keyboards/preonic/keymaps/stathissideris/keymap.c
+++ keyboards/preonic/keymaps/stathissideris/keymap.c
@@ -183,11 +183,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_preonic_grid(
-  RGB_TOG, RGB_MOD, RGB_RMOD, RGB_VAD, RGB_VAI, RGB_HUD, RGB_HUI,   KC_F8,    KC_F9, KC_F10, RGB_M_P, KC_F12,
-  _______, RESET,   DEBUG,    _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL,
-  _______, _______, MU_MOD,   AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______,
-  _______, MUV_DE,  MUV_IN,   MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
-  _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______
+  /* RGB_TOG, RGB_MOD, RGB_RMOD, RGB_VAD, RGB_VAI, RGB_HUD, RGB_HUI,   KC_F8,    KC_F9, KC_F10, RGB_M_P, KC_F12, */
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL,
+  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______,
+  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 [_ASCEND] = LAYOUT_preonic_grid(
